270a5c70d35bd7fbf1e2967ea1db88514b9ac46d
Shiyukine <benjbres@gmail.com>
2026-09-25 00:38:27 +0200

    since someone else leaked the keys, i'm making pulic this repo

---
+++ README.md
@@ -0,0 +1,19 @@
+# Hiiragi-cast
+
+A Python & Electron implementation of a Chromecast receiver.
+
+This app uses [CastReceiver](https://play.google.com/store/apps/details?id=com.softmedia.receiver.castapp&hl=en_US) as a base and create a new receiver with a custom UI and additional features.
+
+## How to use
+1. Install [Python 3](https://www.python.org/downloads/) and [Node.js](https://nodejs.org/en/download/)
+2. Use the command `python setup.py` to install the required Python packages, nodejs packages, setup EVS.
+3. Use the command `python main.py` to restart the receiver after setup.
+
+## What is working and not working
+- Working:
+    - Players that use the standard media player 
+    - Youtube
+    - Tab/desktop casting
+    - DRM protected content
+- Not working:
+    - Players that use their own namespace (e.g. Spotify, Netflix, Disney+)
--- setup.py
+++ setup.py
@@ -20,15 +20,16 @@ if __name__ == "__main__":
         print("If you are unsure how to do this, please refer to the documentation: https://github.com/castlabs/electron-releases/wiki/EVS")
         print("If you do not sign the executable, video playback might not work on Windows and macOS.")
         print("="*90)
-        have_evs_acc = input("Do you have a Castlabs EVS account? (Note: If you say no, you will be asked to sign up.) ([y]/n): ")
+        have_evs_acc = input("Do you have a Castlabs EVS account? (Note: If you say no, you will be asked to sign up. Use 's' to skip.) ([y]/n/s): ")
 
         if have_evs_acc.lower() in ["n", "no"]:
             os.system(f"{sys.executable} -m castlabs_evs.account signup")
 
-        if sys.platform == "win32":
-            os.system(f"{sys.executable} -m castlabs_evs.vmp sign-pkg .\\electron-player\\node_modules\\electron\\dist")
-        elif sys.platform == "darwin":
-            os.system(f"{sys.executable} -m castlabs_evs.vmp sign-pkg ./electron-player/node_modules/electron/dist/")
+        if have_evs_acc.lower() not in ["s", "skip"]:
+            if sys.platform == "win32":
+                os.system(f"{sys.executable} -m castlabs_evs.vmp sign-pkg .\\electron-player\\node_modules\\electron\\dist")
+            elif sys.platform == "darwin":
+                os.system(f"{sys.executable} -m castlabs_evs.vmp sign-pkg ./electron-player/node_modules/electron/dist/")
 
     current_dir = os.getcwd()
 
@@ -60,6 +61,21 @@ if __name__ == "__main__":
         os.chdir(current_dir)
 
     if sys.platform == "linux":
-        os.system(f"pkexec apt-get install libportaudio2")
+        print("="*90)
+        print("WARNING: In order to cast screen, you must install the libportaudio2 package.")
+        print("="*90)
+        installNow = input("Do you want to install it now? ([y]/n): ")
+        if installNow.lower() in ["y", "yes", ""]:
+            # check package manager
+            if os.path.exists("/usr/bin/apt-get"):
+                os.system(f"pkexec apt-get install libportaudio2")
+            elif os.path.exists("/usr/bin/pacman"):
+                os.system(f"pkexec pacman -S libportaudio")
+            elif os.path.exists("/usr/bin/yum"):
+                os.system(f"pkexec yum install libportaudio2")
+            else:
+                print("="*90)
+                print("WARNING: Could not find a supported package manager. Please install libportaudio2 manually.")
+                print("="*90)
 
     os.system(f"{sys.executable} main.py")
